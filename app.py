2bb66ed228549651db60d3e3024d3cf6d5e95720
Yuichi Kuroda <yuichi.kuroda@icloud.com>
2024-08-24 11:53:30 -0700

    Stream + store messages

--- app.py
+++ app.py
@@ -13,15 +13,23 @@ model_kwargs = {"model": "chatgpt-4o-latest", "temperature": 1.2, "max_tokens": 
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    # Your custom logic goes here...
+    # Maintain an array of messages in the user session
+    message_history = cl.user_session.get("message_history", [])
+    message_history.append({"role": "user", "content": message.content})
 
-    response = await client.chat.completions.create(
-        messages=[{"role": "user", "content": message.content}], **model_kwargs
+    response_message = cl.Message(content="")
+    await response_message.send()
+
+    # Pass in the full message history for each request
+    stream = await client.chat.completions.create(
+        messages=message_history, stream=True, **model_kwargs
     )
+    async for part in stream:
+        if token := part.choices[0].delta.content or "":
+            await response_message.stream_token(token)
 
-    # https://platform.openai.com/docs/guides/chat-completions/response-format
-    response_content = response.choices[0].message.content
+    await response_message.update()
 
-    await cl.Message(
-        content=response_content,
-    ).send()
+    # Record the AI's response in the history
+    message_history.append({"role": "assistant", "content": response_message.content})
+    cl.user_session.set("message_history", message_history)
